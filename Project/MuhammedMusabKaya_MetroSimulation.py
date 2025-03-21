4f2a46a4d8b67fca474eab6adca3b1382922ee3d
Muhammed Musab Kaya <032180115@ogr.uludag.edu.tr>
2025-03-21 16:34:33 +0300

    Continued script.

--- Project/MuhammedMusabKaya_MetroSimulation.py
+++ Project/MuhammedMusabKaya_MetroSimulation.py
@@ -3,5 +3,10 @@ import heapq
 from typing import Dict, List, Set, Tuple, Optional
 
 class Station:
-    def __init__(self, idx: str, ):
+    def __init__(self, idx: str, name: str, line: str):
         self.idx = idx
+        self.name = name
+        self.line = line
+        self.neighbors: List[Tuple["Station", int]] = [] # (Station, time) tuples
+
+        
